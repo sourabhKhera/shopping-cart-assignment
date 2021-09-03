a0e525cc0e523c58f24ebe8f559d00794224cd46
Sourabh Khera <soukhera@WKMZT8E660A7.global.publicisgroupe.net>
2021-09-04 03:40:48 +0530

    tablet design for products page done

--- src/containers/pages/products/index.js
+++ src/containers/pages/products/index.js
@@ -13,6 +13,54 @@ const ProductsPage = () => {
   useEffect(() => {
     dispatch(getProducts())
   }, [])
+
+  const mediaQuery = window.matchMedia('(max-width: 768px)')
+
+  const renderProductsInfo = (item) => {
+    return mediaQuery.matches ? (
+      <Fragment>
+        <div className={classes['main__section-right__product-info']}>
+          <div
+            className={classes['main__section-right__product-info__div-elem1']}
+          >
+            <img
+              src={item.imageURL}
+              alt={item.name}
+              className={classes['main__section-right__img']}
+            />
+            <div className={classes['main__section-right__div-elem3']}>
+              <div className={classes['main__section-right__div-elem4']}>
+                {item.description}
+              </div>
+            </div>
+          </div>
+          <Button
+            btnText={`Buy Now @ Rs.${item.price}`}
+            buttonStyle={{ width: '100%', marginTop: '10px' }}
+          />
+        </div>
+      </Fragment>
+    ) : (
+      <Fragment>
+        <img
+          src={item.imageURL}
+          alt={item.name}
+          className={classes['main__section-right__img']}
+        />
+        <div className={classes['main__section-right__div-elem3']}>
+          <div className={classes['main__section-right__div-elem4']}>
+            {item.description}
+          </div>
+        </div>
+        <div className={classes['main__section-right__div-elem5']}>
+          <div className={classes['main__section-right__div-elem6']}>
+            MRP Rs.{item.price}
+          </div>
+          <Button btnText="Buy Now" buttonStyle={{ padding: '10px 25px' }} />
+        </div>
+      </Fragment>
+    )
+  }
   const renderCategories = categories?.length
     ? categories.map((item, idx) => {
         return (
@@ -28,29 +76,11 @@ const ProductsPage = () => {
   const renderProducts = products?.length
     ? products.map((item, idx) => {
         return (
-          <div className={classes['main__section-right__div-elem1']}>
+          <div className={classes['main__section-right__div-elem1']} key={idx}>
             <div className={classes['main__section-right__div-elem2']}>
               {item.name}
             </div>
-            <img
-              src={item.imageURL}
-              alt={item.name}
-              className={classes['main__section-right__img']}
-            />
-            <div className={classes['main__section-right__div-elem3']}>
-              <div className={classes['main__section-right__div-elem4']}>
-                {item.description}
-              </div>
-            </div>
-            <div className={classes['main__section-right__div-elem5']}>
-              <div className={classes['main__section-right__div-elem6']}>
-                MRP Rs.{item.price}
-              </div>
-              <Button
-                btnText="Buy Now"
-                buttonStyle={{ padding: '10px 25px' }}
-              />
-            </div>
+            {renderProductsInfo(item)}
           </div>
         )
       })
